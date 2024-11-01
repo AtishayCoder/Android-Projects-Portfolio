fd533c0ae55db32b08c0b4558f6dd2f8d26fc518
Atishay <v.kandarp@gmail.com>
2024-11-01 18:39:33 +0530

    Files have been uploaded

---
+++ app/src/main/java/com/example/myandroidprojects/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.myandroidprojects;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView list = findViewById(R.id.recycler_view_projects);
+
+        Project[] projects = {
+                new Project("Getting Started App", "My very first android project, 'The Hello World' app.", R.drawable.getting_started),
+                new Project("Quote App", "I learned about the fundamentals of xml and layouts in Android.", R.drawable.quote),
+                new Project("BMI Calculator", "A classic. Calculates a person's BMI. I learnt about maths, variables and methods in Java.", R.drawable.tape),
+                new Project("Inches To Meters Converter", "Converts inches to meters using a simple formula.", R.drawable.bmi_calculator),
+                new Project("Gola Sizzlers (Restaurant App)", "A simple app for a restaurant. Shows the user about what is available in the eatery. I learnt about Intents, ListView and CardView.", R.drawable.hungry_developer),
+                new Project("Bucket List App", "An app that lists all off the things I want to do and the places I want to visit.", R.drawable.bucket_list),
+                new Project("Self Promo App", "An app that helps you to create a self-promo message. I learnt about kotlin, checkboxes and styled edit texts.", R.drawable.self_promo),
+                new Project("Registration app", "An app that can register you for a service. I practiced implicit intents and creating stylish forms.", R.drawable.registration),
+                new Project("Record Keeper", "It keeps track of your running and cycling records. I learnt about fragments and the bottom navigation bar widget.", R.drawable.record_keeper),
+                new Project("Calculator", "It is a simple calculator app made with Kotlin.", R.drawable.calculator
+        };
+
+        ProjectsAdapter adapter = new ProjectsAdapter(projects);
+        list.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/com/example/myandroidprojects/Project.java
@@ -0,0 +1,13 @@
+package com.example.myandroidprojects;
+
+public class Project {
+    String projectName;
+    String projectDescription;
+    int image;
+
+    public Project(String projectName, String projectDescription, int image) {
+        this.projectName = projectName;
+        this.projectDescription = projectDescription;
+        this.image = image;
+    }
+}
